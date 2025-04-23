c1399b49f5798248880448da821e45cc1b64179c
Shohei <yukawa.shouhei@gmail.com>
2025-04-23 19:23:28 +0900

    Initial Commit

---
+++ OnboardingBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingBootcamp
+//
+//  Created by 湯川昇平 on 2025/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingBootcamp/OnboardingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingBootcampApp.swift
+//  OnboardingBootcamp
+//
+//  Created by 湯川昇平 on 2025/04/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
