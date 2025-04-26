61b599210ec76ffedd478453a52a92fafd129948
Shohei <yukawa.shouhei@gmail.com>
2025-04-26 16:58:11 +0900

    gender selection 作成

--- OnboardingBootcamp/OnboradingViews/OnboardingView.swift
+++ OnboardingBootcamp/OnboradingViews/OnboardingView.swift
@@ -16,7 +16,11 @@ struct OnboardingView: View {
      3 - Add gender
      */
     
-    @State var onboardingState: Int = 1
+    @State var onboardingState: Int = 3
+    
+    @State var name: String = ""
+    @State var age: Double = 50
+    @State var gender: String = ""
     
     var body: some View {
         ZStack {
@@ -27,6 +31,10 @@ struct OnboardingView: View {
                     welcomeSection
                 case 1:
                     addNameSection
+                case 2:
+                    addAgeSection
+                case 3:
+                    addGenderSection
                 default:
                     RoundedRectangle(cornerRadius: 25.0)
                         .foregroundStyle(Color.green)
@@ -94,17 +102,79 @@ extension OnboardingView {
         .padding(30)
     }
     
+    
     private var addNameSection: some View {
-        VStack(spacing: 40) {
+        VStack(spacing: 20) {
             Spacer()
             Text("What's your name")
                 .font(.largeTitle)
                 .fontWeight(.semibold)
                 .foregroundStyle(.white)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(Color.white)
+                .cornerRadius(10)
             Spacer()
             Spacer()
         }
-        .multilineTextAlignment(.center)
+        .padding(30)
+    }
+    
+    private var addAgeSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What's your age?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            
+            Text("\(String(format: "%.0f", age))")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            Slider(value: $age, in: 18...100, step: 1)
+                .tint(.white)
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+    
+    private var addGenderSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            Text("What's your gender?")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+            
+            Menu {
+                Picker("Options", selection: $gender) {
+                    Text("Male").tag("Male")
+                    Text("Female").tag("Female")
+                    Text("Non-Binary").tag("Non-Binary")
+                }
+            } label: {
+                
+                HStack {
+                    
+                    Text(gender.isEmpty ? "Select a gender" : gender)
+                    
+                }
+                .font(.headline)            // フォント
+                .foregroundStyle(.purple)   // 文字色
+                .frame(height: 55)            // 高さ
+                .frame(maxWidth: .infinity) // 最大幅
+                .padding(.horizontal)      // 左右のパディング (テキストが長くなった時用)
+                .background(Color.white)    // 背景色
+                .cornerRadius(10)           // 角丸
+            }
+            
+            Spacer()
+            Spacer()
+        }
         .padding(30)
     }
 }
