900738b48c42734a78ef862379ce7ab9b7c00037
Shohei <yukawa.shouhei@gmail.com>
2025-04-26 14:41:15 +0900

    welcome section 完成

--- OnboardingBootcamp/OnboradingViews/OnboardingView.swift
+++ OnboardingBootcamp/OnboradingViews/OnboardingView.swift
@@ -8,11 +8,87 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    /* Onboarding States:
+     0 - Welcome screen
+     1 - Add name
+     2 - Add age
+     3 - Add gender
+     */
+    
+    @State var onboardingState: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // content
+            ZStack {
+                switch onboardingState {
+                case 0:
+                    welcomeSection
+                default:
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .foregroundStyle(Color.green)
+                }
+            }
+            //buttons
+            VStack {
+                Spacer()
+                bottomButton
+            }
+            .padding(30)
+        }
+        .background(Color.purple)
     }
+    
+    
 }
 
 #Preview {
     OnboardingView()
+}
+
+// Mark: COMPONENTS
+
+extension OnboardingView {
+    
+    private var bottomButton: some View {
+        Text("Sign in")
+            .font(.headline)
+            .foregroundStyle(.purple)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(10)
+            .onTapGesture {
+                
+            }
+    }
+    private var welcomeSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            Image(systemName: "heart.text.square.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200,height: 200)
+                .foregroundColor(.white)
+            Text("find your match.")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .overlay(
+                    Capsule(style: .continuous)
+                        .frame(height: 3)
+                        .offset(y: 5)
+                        .foregroundColor(.white)
+                    , alignment: .bottom
+                )
+            Text("This is the #1 app for finding your match online! In this tutorial wea re practicing using Appstorage and other SwiftUI techniques.")
+                .fontWeight(.medium)
+                .foregroundStyle(.white)
+            Spacer()
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .padding(30)
+    }
 }
